591921a2df9b72e1c94b1c44a517054e537c50b5
Yiqun Zhang <zhangyiqun9164@gmail.com>
2016-02-17 08:32:15 -0600

    Remove SparkR script and make progress in Scala

--- scripts/spark/gamma.scala
+++ scripts/spark/gamma.scala
@@ -4,11 +4,11 @@ import org.apache.spark.SparkConf
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.mllib.linalg.Vectors
 
-val filePath: String = ("hdfs://node1:54310/KDDn001Md38_Y.csv")
-val rows = sc.textFile(filePath).map { line =>
+val filePath: String = ("hdfs://node1:54310/KDDn100Kd38_Y.csv")
+val Z = sc.textFile(filePath).map { line =>
   val values = line.split(',').map(_.toDouble)
-  Vectors.dense(values)
+  Vectors.dense(Array[Double](1.0) ++ values)
 }
-val mat = new RowMatrix(rows)
-val Gamma = mat.computeGramianMatrix()
-println("Gamma:\n" + Gamma)
+val matZ = new RowMatrix(Z)
+val Gamma = matZ.computeGramianMatrix()
+println("Gamma:\n" + Gamma) 
