611ccd49cb4354e6b0fbc3de985bf0c39feb08c7
Yiqun Zhang <zhangyiqun9164@gmail.com>
2016-05-11 12:51:36 -0500

    Cut in half

--- GPUDenseGamma/funcs.cpp
+++ GPUDenseGamma/funcs.cpp
@@ -23,10 +23,10 @@ void computeGamma(double *memChunk, double *Gamma, size_t d, size_t nChunkSize) 
 void computeGamma(double *restrict memChunk, double *restrict Gamma, size_t d, size_t nChunkSize) {
   #pragma acc update device(memChunk[0:d*nChunkSize])
   #pragma acc kernels loop present(memChunk[0:d*nChunkSize], Gamma[0:d*d]) \
-                           copyin(d, nChunkSize) independent collapse(2)
+                           copyin(d, nChunkSize) independent
 #endif
   for (size_t j=0; j<d; j++) {
-    for (size_t k=0; k<d; k++) {
+    for (size_t k=0; k<=j; k++) {
       double GammaDelta = 0.0;
       #ifndef NOGPU
       #pragma acc loop reduction(+:GammaDelta) vector
