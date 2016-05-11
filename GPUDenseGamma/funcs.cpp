aa1e05bf82b1f10d2fdcd4ed7b396b58e48f657c
Yiqun Zhang <zhangyiqun9164@gmail.com>
2016-05-11 10:14:40 -0500

    Parallelize GammaDelta

--- GPUDenseGamma/funcs.cpp
+++ GPUDenseGamma/funcs.cpp
@@ -5,9 +5,12 @@ void computeGamma(double *memChunk, double *Gamma, size_t d, size_t nChunkSize) 
   #pragma acc kernels loop present(memChunk[0:d*nChunkSize], Gamma[0:d*d], d, nChunkSize) independent collapse(2)
   for (size_t j=0; j<d; j++) {
     for (size_t k=0; k<d; k++) {
+      double GammaDelta = 0.0;
+      #pragma acc loop reduction(+:GammaDelta)
       for (size_t i=0; i<nChunkSize; i++) {
-        Gamma[j*d+k] += memChunk[j*nChunkSize+i] * memChunk[k*nChunkSize+i];
+        GammaDelta += memChunk[j*nChunkSize+i] * memChunk[k*nChunkSize+i];
       }
+      Gamma[j*d+k] += GammaDelta;
     }
   }
   #pragma acc exit data copyout(Gamma[0:d*d]) delete(memChunk[0:d*nChunkSize])
