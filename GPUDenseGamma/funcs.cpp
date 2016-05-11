1f6f51c0c8d5ec25d5610701982cb2faae33b946
Yiqun Zhang <zhangyiqun9164@gmail.com>
2016-05-11 11:16:30 -0500

    ADD restrict, gang, worker, vector

--- GPUDenseGamma/funcs.cpp
+++ GPUDenseGamma/funcs.cpp
@@ -1,12 +1,12 @@
 #include <cstdint>
 using namespace std;
-void computeGamma(double *memChunk, double *Gamma, size_t d, size_t nChunkSize) {
+void computeGamma(double *restrict memChunk, double *restrict Gamma, size_t d, size_t nChunkSize) {
   #pragma acc enter data copyin(memChunk[0:d*nChunkSize], Gamma[0:d*d], d, nChunkSize)
-  #pragma acc kernels loop present(memChunk[0:d*nChunkSize], Gamma[0:d*d], d, nChunkSize) independent collapse(2)
+  #pragma acc kernels loop present(memChunk[0:d*nChunkSize], Gamma[0:d*d], d, nChunkSize) independent gang worker collapse(2)
   for (size_t j=0; j<d; j++) {
     for (size_t k=0; k<d; k++) {
       double GammaDelta = 0.0;
-      #pragma acc loop reduction(+:GammaDelta)
+      #pragma acc loop reduction(+:GammaDelta) vector
       for (size_t i=0; i<nChunkSize; i++) {
         GammaDelta += memChunk[j*nChunkSize+i] * memChunk[k*nChunkSize+i];
       }
