d37accc0fa31cd34e9127ea577d897f462f62ce9
Yiqun Zhang <zhangyiqun9164@gmail.com>
2016-05-11 00:40:20 -0500

    Working version 1

--- GPUDenseGamma/funcs.cpp
+++ GPUDenseGamma/funcs.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 void computeGamma(double *memChunk, double *Gamma, size_t d, size_t nChunkSize) {
   #pragma acc enter data copyin(memChunk[0:d*nChunkSize], Gamma[0:d*d], d, nChunkSize)
-  #pragma acc kernels loop present(memChunk[0:d*nChunkSize], Gamma[0:d*d], d, nChunkSize)
+  #pragma acc kernels loop present(memChunk[0:d*nChunkSize], Gamma[0:d*d], d, nChunkSize) independent collapse(2)
   for (size_t j=0; j<d; j++) {
-    #pragma acc loop
-    for (size_t k=0; k<=j; k++) {
-      #pragma acc loop
+    for (size_t k=0; k<d; k++) {
       for (size_t i=0; i<nChunkSize; i++) {
         Gamma[j*d+k] += memChunk[j*nChunkSize+i] * memChunk[k*nChunkSize+i];
       }
     }
   }
-  #pragma acc exit data copyout(Gamma[0:d*d])
+  #pragma acc exit data copyout(Gamma[0:d*d]) delete(memChunk[0:d*nChunkSize])
 }
